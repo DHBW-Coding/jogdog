30581ed28a78b4dc053060810c374978418d24eb
Jonas Berger <94634381+JGlumanda@users.noreply.github.com>
2023-02-15 15:40:02 +0100

    Fixed that the Theme is not displayed
    Signed-off-by: Jonas Berger <94634381+JGlumanda@users.noreply.github.com>

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.lightBlue, secondaryContainer: Colors.orange, brightness: Brightness.light),
+        seedColor: Colors.lightBlue, secondaryContainer: Colors.orangeAccent, brightness: Brightness.light),
     useMaterial3: true,
   );
 
   static final ThemeData darkTheme = ThemeData(
     colorScheme: ColorScheme.fromSeed(
-        seedColor: Colors.blue, secondaryContainer: Colors.orange, brightness: Brightness.dark),
+        seedColor: Colors.blueAccent, secondaryContainer: Colors.orange, brightness: Brightness.dark),
     useMaterial3: true,
   );
 }
