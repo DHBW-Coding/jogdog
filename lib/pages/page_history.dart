0449737dc950470ad61b166184c79a518c932d9b
JGlumanda <jonas-berger@hotmail.com>
2023-02-22 13:07:15 +0100

    Added a message when no session is found
    Signed-off-by: JGlumanda <jonas-berger@hotmail.com>

--- lib/pages/page_history.dart
+++ lib/pages/page_history.dart
@@ -21,73 +21,82 @@ class _HistoryState extends State<History> {
       appBar: AppBar(
         title: const Text("History"),
       ),
-      body: ListView.separated(
-        separatorBuilder: (BuildContext context, int index) =>
-            const SizedBox(height: 5),
-        itemCount: SessionManager().sessions.length,
-        itemBuilder: ((context, index) {
-          final session = SessionManager().sessions[index];
-          return SizedBox(
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(5),
-                child: ListTile(
-                  title: Text(SessionManager().getDateAsString(session)),
-                  subtitle: Text(
-                      "${SessionManager().getStartTimeAsString(session)} - ${SessionManager().getEndTimeAsString(session)}"),
-                  trailing: const Icon(Icons.run_circle),
-                  leading: const Icon(Icons.notes_outlined),
-                  onLongPress: () {
-                    setState(
-                      () {
-                        showDialog<void>(
-                          context: context,
-                          barrierDismissible: false, // user must tap button!
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text(
-                                  'You are about to delete this session!'),
-                              content: const Text(
-                                  'Are you sure you want to delete this session? This action cannot be undone.'),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('Cancel'),
-                                  onPressed: () =>
-                                      Navigator.pop(context, "Cancel"),
-                                ),
-                                TextButton(
-                                  child: const Text('Delete'),
-                                  onPressed: () {
-                                    setState(
-                                      () {
-                                        SessionManager()
-                                            .deleteSession(session.id);
-                                        SessionManager().sessions.remove(session);
-                                      },
-                                    );
-                                    Navigator.pop(context, "Delete");
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                    );
-                  },
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: ((context) => RunInsights(session: session)),
+      body: SessionManager().sessions.isEmpty
+          ? ListTile(
+              title: const Text("No sessions found"),
+              subtitle: const Text("Start a session to see it here"),
+            )
+          : ListView.separated(
+              separatorBuilder: (BuildContext context, int index) =>
+                  const SizedBox(height: 5),
+              itemCount: SessionManager().sessions.length,
+              itemBuilder: ((context, index) {
+                final session = SessionManager().sessions[index];
+                return SizedBox(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: ListTile(
+                        title: Text(SessionManager().getDateAsString(session)),
+                        subtitle: Text(
+                            "${SessionManager().getStartTimeAsString(session)} - ${SessionManager().getEndTimeAsString(session)}"),
+                        trailing: const Icon(Icons.run_circle),
+                        leading: const Icon(Icons.notes_outlined),
+                        onLongPress: () {
+                          setState(
+                            () {
+                              showDialog<void>(
+                                context: context,
+                                barrierDismissible:
+                                    false, // user must tap button!
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text(
+                                        'You are about to delete this session!'),
+                                    content: const Text(
+                                        'Are you sure you want to delete this session? This action cannot be undone.'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        child: const Text('Cancel'),
+                                        onPressed: () =>
+                                            Navigator.pop(context, "Cancel"),
+                                      ),
+                                      TextButton(
+                                        child: const Text('Delete'),
+                                        onPressed: () {
+                                          setState(
+                                            () {
+                                              SessionManager()
+                                                  .deleteSession(session.id);
+                                              SessionManager()
+                                                  .sessions
+                                                  .remove(session);
+                                            },
+                                          );
+                                          Navigator.pop(context, "Delete");
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                          );
+                        },
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: ((context) =>
+                                  RunInsights(session: session)),
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
-              ),
+                    ),
+                  ),
+                );
+              }),
             ),
-          );
-        }),
-      ),
     );
   }
 }
