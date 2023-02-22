e6d29ec4c16b48e9be584317efa2fac44fd26804
JGlumanda <jonas-berger@hotmail.com>
2023-02-22 16:10:36 +0100

    final changes to the theme selector --> Now the options are placed in a card
    Signed-off-by: JGlumanda <jonas-berger@hotmail.com>

--- lib/widgets/settings_widgets/theme_selector.dart
+++ lib/widgets/settings_widgets/theme_selector.dart
@@ -11,7 +11,7 @@ class ThemeSelectorState extends State<ThemeSelector> {
   final Map<String, IconData> themes = {
     "Automatic": Icons.brightness_auto,
     "Dark Theme": Icons.brightness_3,
-    "Light Theme": Icons.brightness_medium
+    "Light Theme": Icons.brightness_medium,
   };
 
   late String _selectedTheme = "Automatic";
@@ -28,36 +28,43 @@ class ThemeSelectorState extends State<ThemeSelector> {
           context: context,
           builder: (BuildContext context) {
             return StatefulBuilder(
-              builder: (context, setThemeState) => SizedBox(
-                height: themes.length * 100,
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: themes.length,
-                    itemBuilder: (context, index) {
-                      String theme = themes.keys.elementAt(index);
-                      return RadioListTile(
-                        value: theme.toString(),
-                        groupValue: _selectedTheme,
-                        controlAffinity: ListTileControlAffinity.trailing,
-                        secondary: Icon(themes[theme]),
-                        onChanged: (value) {
-                          setThemeState(
-                            () {
-                              _selectedTheme = value.toString();
+                builder: (context, setThemeState) => SizedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Card(
+                      elevation: 2,
+                      child: ListView.separated(
+                        shrinkWrap: true,
+                        separatorBuilder: (BuildContext context, int index) =>
+                        const Divider(
+                          height: 5,
+                        ),
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: themes.length,
+                        itemBuilder: (context, index) {
+                          String theme = themes.keys.elementAt(index);
+                          return RadioListTile(
+                            value: theme.toString(),
+                            groupValue: _selectedTheme,
+                            controlAffinity: ListTileControlAffinity.trailing,
+                            secondary: Icon(themes[theme]),
+                            title: Text(
+                              theme,
+                              style: Theme.of(context).textTheme.headlineSmall,
+                            ),
+                            onChanged: (value) {
+                              setThemeState(
+                                    () {
+                                  _selectedTheme = value.toString();
+                                },
+                              );
                             },
                           );
                         },
-                        title: Text(
-                          theme,
-                          style: Theme.of(context).textTheme.headlineSmall,
-                        ),
-                      );
-                    },
+                      ),
+                    ),
                   ),
                 ),
-              ),
             );
           },
         );
